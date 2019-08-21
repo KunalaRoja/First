eac8646899fc1865ff26e4e2933ef242447ce129
Roja Kunala <kroja@idexcel.internal>
2019-08-21 11:43:08 +0530

    MY First PRog inn REpo is commited

---
+++ First.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class First{
+
+	public static void main(String[] args)
+	{
+		
+		for(int i=0; i<=10;i++)
+		{
+
+			System.out.println("HELLo JAVA with JEnkins"+ i);
+		}
+		System.out.println("PRG is passed");
+	}
+}
